0f31565326b238b0367e09a266b2f0f97ba74860
sasha <shaparev1992@gmail.com>
2017-08-14 11:45:15 +0300

    init

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<html>
-<head>
-    <title>Пример</title>
-</head>
-<body>
-
-<h1>Привет</h1>
-
-</body>
-</html>
+<?php
+echo "<html><head>";
+echo '<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">';
+echo "<title>Мой первый PHP скрипт</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Привет! Я PHP скрипт!</h1>";
+echo "</body>";
+echo "</html>";
+?>
